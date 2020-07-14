88f1de02e7dc22f922d51d3d787c70ae78e3f7e3
Felipe Zanetti <felipeajz@hotmail.com>
2020-07-14 20:24:28 -0300

    Template TypeScript adicionado

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#6C63FF" />
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#EEE',
-        }}
-      >
-      </View>
-    </>
-  );
-};
+// import { Container } from './styles';
 
-export default App;
+const src: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#6C63FF" />
+    <View style={{ flex: 1, backgroundColor: '#DFDFDF' }} />
+  </>
+);
+
+export default src;
